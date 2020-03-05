4e92f26d33c5ee604c3ed9c0a0c5f21c7c156c50
Stefanos <stefanospapadam@gmail.com>
2020-03-06 00:12:29 +0200

    Notes first try

--- Graphics2019/part 1/triPaintFlat.m
+++ Graphics2019/part 1/triPaintFlat.m
@@ -9,104 +9,76 @@ function Y = triPaintFlat(X,V,C)
     g = (C(1,2)+C(2,2)+C(3,2))/3;
     b = (C(1,3)+C(2,3)+C(3,3))/3;
     %compute xmin,xmax,ymin,ymax for each side of the triangle
- for i=0:1:2
-     if i ~= 2
-            xmin(i+1) = min(V(i+1:i+2,1));
-            xmax(i+1) = max(V(i+1:i+2,1));
-            ymin(i+1) = min(V(i+1:i+2,2));
-            ymax(i+1) = max(V(i+1:i+2,2));
+ for i=1:3
+     if i ~= 3
+            xmin(i) = min(V(i:i+1,1));
+            xmax(i) = max(V(i:i+1,1));
+            ymin(i) = min(V(i:i+1,2));
+            ymax(i) = max(V(i:i+1,2));
      else 
-            xmin(i+1) = min(V(i+1,1),V(i-1,1));
-            xmax(i+1) = max(V(i+1,1),V(i-1,1));
-            ymin(i+1) = min(V(i+1,2),V(i-1,2));
-            ymax(i+1) = max(V(i+1,2),V(i-1,2));
+            xmin(i) = min(V(i,1),V(i-2,1));
+            xmax(i) = max(V(i,1),V(i-2,1));
+            ymin(i) = min(V(i,2),V(i-2,2));
+            ymax(i) = max(V(i,2),V(i-2,2));
      end
  end
  y_minimum = min(ymin(:));%find y_min from the whole triangle 
  y_maximum = max(ymax(:));%find y_max from the whole triangle
  %compute and store active edges for ymin scanline
-
- for i = 1:3
-    x = find(V(:,2) == ymin(i));
-    m = (ymax(i) - ymin(i))/(xmax(i) - xmin(i));
-    edgebucket(i) = struct('yMax',ymax(i),'x',V(x,1),'slope',m);
- end
- for i = 1:1200
-     edgeTable(i) = struct('yMax1', 0 ,'x1', 0 ,'slope1', 0 , 'yMax2', 0 ,'x2', 0 ,'slope2', 0);
-     for j = 1:3
-      if i == ymin(j)
-        if edgeTable(i).yMax1 == 0
-            edgeTable(i).yMax1 = edgebucket(j).yMax;
-            edgeTable(i).x1 = edgebucket(j).x;
-            edgeTable(i).slope1 = edgebucket(j).slope;
-        else
-            edgeTable(i).yMax2 = edgebucket(j).yMax;
-            edgeTable(i).x2 = edgebucket(j).x;
-            edgeTable(i).slope2 = edgebucket(j).slope;
-        end
-      end
-     end
- end
- for i=y_minimum:y_maximum
-     if edgeTable(i).x2 < edgeTable(i).x1
-        [edgeTable(i).yMax1,edgeTable(i).yMax2] = deal(edgeTable(i).yMax1,edgeTable(i).yMax2);
-        [edgeTable(i).x1,edgeTable(i).x2] = deal(edgeTable(i).x1,edgeTable(i).x2);
-        [edgeTable(i).slope1,edgeTable(i).slope2] = deal(edgeTable(i).slope1,edgeTable(i).slope2);
-     end
- end
  
- %k = 1;
- %edge = zeros(2,1);%edge has two numbers 0 to 2 which indicates  
+ k = 1;
+ activeEdges = zeros(2,1);%edge has two numbers 0 to 2 which indicates  
  %which side is active in every step 
- %array has the coordinates of sides' peaks 
- %array = [V(1,1) V(1,2) V(2,1) V(2,2);V(2,1) V(2,2) V(3,1) V(3,2);V(3,1) V(3,2) V(1,1) V(1,2)];
- %for i=1:3
- %   if array(i,2)==y_minimum || array(i,4)==y_minimum
- %       edge(k) = i-1;
- %       k = k + 1;
- %   end
- %end
+ %array has the coordinates of sides' peaks
+ m1 = (V(2,2) - V(1,2))/(V(2,1)-V(1,1));
+ m2 = (V(3,2) - V(2,2))/(V(3,1)-V(2,1));
+ m3 = (V(1,2) - V(3,2))/(V(1,1)-V(3,1));
+ 
+ array(1) = struct('x1',V(1,1),'y1',V(1,2),'x2',V(2,1),'y2',V(2,2),'m',m1);
+ array(2) = struct('x1',V(2,1),'y1',V(2,2),'x2',V(3,1),'y2',V(3,2),'m',m2);
+ array(3) = struct('x1',V(3,1),'y1',V(3,2),'x2',V(1,1),'y2',V(1,2),'m',m3);
+ activePoints = struct('x1',0,'y1',0,'m1',0,'x2',0,'y2',0,'m2',0);
+        
+ for i=1:3
+    if array(i).y1==y_minimum
+        activePoints.x1 = array(i).x1;
+        activePoints.y1 = array(i).y1;
+        activePoints.m1 = array(i).m;
+        activeEdges(k) = i;
+        k = k + 1;
+    elseif array(i).y2==y_minimum
+        activePoints.x2 = array(i).x2;
+        activePoints.y2 = array(i).y2;
+        activePoints.m2 = array(i).m;
+        activeEdges(k) = i;
+        k = k + 1;
+    end
+ end
  %compute the slope of each active side
- %m1 = (array(edge(1)+1,4)-array(edge(1)+1,2))/(array(edge(1)+1,3)-array(edge(1)+1,1));
- %m2 = (array(edge(2)+1,4)-array(edge(2)+1,2))/(array(edge(2)+1,3)-array(edge(2)+1,1));
+
 %compute active points for scanline 
-%if array(edge(1)+1,2) == y_minimum
- %   x_minimum = array(edge(1)+1,1);
-%elseif array(edge(1)+1,4) == y_minimum
- %   x_minimum = array(edge(1)+1,3);
-%end
-%active_points.x1 = x_minimum;
-%active_points.y1 = y_minimum;
-%active_points.x2 = x_minimum;
-%active_points.y2 = y_minimum;
+
 
 %apply scanline algorithm
-ActiveEdgeTable = struct('yMax1', edgeTable(y_minimum).yMax1 ,'x1',  edgeTable(y_minimum).x1 ,'slope1',  edgeTable(y_minimum).slope1 , 'yMax2',  edgeTable(y_minimum).yMax2 ,'x2',  edgeTable(y_minimum).x2 ,'slope2', edgeTable(y_minimum).slope2);
+
 for y=y_minimum:y_maximum 
 
-    if ActiveEdgeTable.x2 < ActiveEdgeTable.x1
-        [ActiveEdgeTable.yMax1,ActiveEdgeTable.yMax2] = deal(ActiveEdgeTable.yMax1,ActiveEdgeTable.yMax2);
-        [ActiveEdgeTable.x1,ActiveEdgeTable.x2] = deal(ActiveEdgeTable.x1,ActiveEdgeTable.x2);
-        [ActiveEdgeTable.slope1,ActiveEdgeTable.slope2] = deal(ActiveEdgeTable.slope1,ActiveEdgeTable.slope2);
-     end
     %sort active points by increasing x
-    
-    %if active_points.x2 < active_points.x1
-     %   [active_points.x2 , active_points.x1] = deal(active_points.x2,active_points.x1);
-        %[edge(1),edge(2)] = deal (edge(2),edge(1));
-        %[m1,m2] = deal(m2,m1);
-        %active_points.x2 = 
-    %end
+
+    if activePoints.x2 < activePoints.x1
+        [activePoints.x2 , activePoints.x1] = deal(activePoints.x2,activePoints.x1);
+        [activePoints.y2 , activePoints.y1] = deal(activePoints.y2,activePoints.y1);
+    end
     cross_count = 0;
     %scan the image
     set = 0;
     for x=1:1200
          %if active point found increase cross_count 
-        if x >= ActiveEdgeTable.x1 & set == 0
+        if x >= activePoints.x1 && set == 0
             set = 1; 
             cross_count = cross_count +1;      
-        elseif   x >= ActiveEdgeTable.x2
-            cross_count = cross_count +1;
+        elseif   x >= activePoints.x2
+             cross_count = cross_count +1;
         end
         %if cross_count is odd number then x is located between active
         %points so this point is painted
@@ -119,25 +91,49 @@ for y=y_minimum:y_maximum
         %if x surpass the second active point we break the loop 
         %or the second point is Inf 
         %this case exists when m1=0 or m2=0 so we break the loop 
-        if x>ActiveEdgeTable.x2 | x == Inf
+        if x>activePoints.x2 || x == Inf
              break;
         end
     end
    
-%     count=2;
+    count=0;
     %track active edges
     %when y reaches minimum value of ymax array then we should change active sides
-    whole1 = ActiveEdgeTable.x1;
-    whole2 = ActiveEdgeTable.x2;
-    if y+1 == ActiveEdgeTable.yMax1
-       ActiveEdgeTable.yMax1 = edgeTable(y+1).yMax1;
-       ActiveEdgeTable.x1 = edgeTable(y+1).x1;
-       ActiveEdgeTable.slope1 = edgeTable(y+1).slope1;
-     elseif y+1 == ActiveEdgeTable.yMax2
-       ActiveEdgeTable.yMax2 = edgeTable(y+1).yMax2;
-       ActiveEdgeTable.x2 = edgeTable(y+1).x2;
-       ActiveEdgeTable.slope2 = edgeTable(y+1).slope2;
-
+    if y+1 == max(ymin(:))
+        k = 1;
+        index = zeros(2,1);
+        newActiveEdges = zeros(2,1);
+        for i=1:3
+            if array(i).y1 == max(ymin(:)) || array(i).y2 == max(ymin(:))
+               index(k) = i; 
+               k = k + 1;
+            end
+        end
+        k = 1;
+        hist = histcounts([activeEdges;index]);
+        for i = 1:length(hist)
+            if hist(i) == 1
+                newActiveEdges(k) = i;
+                k = k + 1;
+            end
+        end
+        
+        
+        hist = histcounts([array(newActiveEdges(1)).m;
+        array(newActiveEdges(2)).m;
+        array(activeEdges(1)).m;
+        array(activeEdges(2)).m]);
+        for i = 1:length(hist)
+            if hist(i) == 2
+                found = i;
+            end
+        end
+    else
+        activePoints.y1 = activePoints.y1 + 1;
+        activePoints.x1 = activePoints.x1 + 1/activePoints.m1;
+        activePoints.y2 = activePoints.y2 + 1;
+        activePoints.x2 = activePoints.x2 + 1/activePoints.m2;
+    end
 %      ok = 0;
 %      for i=1:3%access array in order to find this value 
 %          index_new = 3;
@@ -169,18 +165,18 @@ for y=y_minimum:y_maximum
 %          end
 %      end
 
-     else%this case is when we don't change active side so we update active
+    % else%this case is when we don't change active side so we update active
      %points
 %         whole1 = whole1 + 1/ActiveEdgeTable.slope1;
 %         whole2 = whole2 + 1/ActiveEdgeTable.slope2;
 %         fract1 = whole1 - fix(whole1);
 %         fract2 = whole2 - fix(whole2);
 %         if fract1 > 1
-            ActiveEdgeTable.x1 = ActiveEdgeTable.x1 + 1/ActiveEdgeTable.slope1;
+    %        ActiveEdgeTable.x1 = ActiveEdgeTable.x1 + 1/ActiveEdgeTable.slope1;
 %             fract1 = fract1 - 1;
 %         end
 %         if fract2 > 1 
-            ActiveEdgeTable.x2 = ActiveEdgeTable.x2 +  1/ActiveEdgeTable.slope2;
+     %       ActiveEdgeTable.x2 = ActiveEdgeTable.x2 +  1/ActiveEdgeTable.slope2;
 %             fract2 = fract2 - 1; 
 %         end
         %active_points.x1 = round(active_points.x1 + 1/m1);
@@ -199,9 +195,9 @@ for y=y_minimum:y_maximum
         
     end
 %       imshow(X)
-     
+   Y = X;  
 end
 %return image
   
-   Y = X;
-end
+   
+%end
