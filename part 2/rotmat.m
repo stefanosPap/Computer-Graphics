9ddaaac7cf4083c9a303ed7c448e5efb531dbbef
Stefanos <stefanospapadam@gmail.com>
2020-03-12 19:46:06 +0200

    Implements rotmat

--- part 2/rotmat.m
+++ part 2/rotmat.m
@@ -1,2 +1,6 @@
 function R = rotmat(theta, u)
+ R = [(1 - cos(theta))*u(1)^2 + cos(theta) (1 - cos(theta))*u(1)*u(2) - sin(theta)*u(3) (1 - cos(theta))*u(1)*u(3) + sin(theta)*u(2);
+      (1 - cos(theta))*u(1)*u(2) + sin(theta)*u(3) (1 - cos(theta))*u(2)^2 + cos(theta)*u(3) (1 - cos(theta))*u(2)*u(3) - sin(theta)*u(1);
+      (1 - cos(theta))*u(1)*u(3) - sin(theta)*u(2) (1 - cos(theta))*u(2)*u(3) + sin(theta)*u(1) (1 - cos(theta))*u(3)^2 + cos(theta)
+     ];
 end
