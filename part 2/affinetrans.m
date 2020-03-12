ca9038c8afac8776246c2d13d4fa791c10a34be1
Stefanos <stefanospapadam@gmail.com>
2020-03-12 20:07:07 +0200

    Implements affinetrans

--- part 2/affinetrans.m
+++ part 2/affinetrans.m
@@ -1,2 +1,8 @@
-function cq = affinetrans(cp , R, ct )
+function cq = affinetrans(cp , R, ct)
+    sizes = size(cp);
+    cq = zeros(sizes(1),sizes(2));
+    for i = 1:sizes(2)
+        cq(:,i) = R * cp(:,i);
+        cq(:,i) = cq(:,i) + ct;
+    end
 end
