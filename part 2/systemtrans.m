e163983ba55f19de435e77a40dba7ac59517d567
Stefanos <stefanospapadam@gmail.com>
2020-03-13 15:56:31 +0200

    Implements systemtrans

--- part 2/systemtrans.m
+++ part 2/systemtrans.m
@@ -1,2 +1,12 @@
-function d_p = systemtrans(cp , b1 , b2 , b3 , c0 )
+function dp = systemtrans(cp , b1 , b2 , b3 , c0 )
+   
+   cp = cp(:);
+   b1 = b1(:);
+   b2 = b2(:);
+   b3 = b3(:);
+   c0 = c0(:);
+   
+   L = [b1';b2';b3'];
+   dp = L \ (cp - c0);
+
 end
