7704c8f7e0425b518cd934ef1792ac7fa4ac9f68
Stefanos <stefanospapadam@gmail.com>
2020-03-14 18:20:34 +0200

    Implements projectCamera

--- part 2/projectCamera.m
+++ part 2/projectCamera.m
@@ -1,2 +1,18 @@
 function [P, D] = projectCamera(w, cv , cx , cy , p)
+    cx = cx(:);
+    cy = cy(:);
+    R = [cx  cy  zeros(3,1)];
+    d = cv(:);
+    s = size(p);
+    P = zeros(2,s(2));
+    D = zeros(s(2),1);
+    for i = 1:s(2)
+        p(:,i) = R' * p(:,i);
+        p(:,i) = -R' * d +p(:,i);
+        x = (w * p(1,i)) / p(3,i);
+        y = (w * p(2,i)) / p(3,i);
+        P(1,i) = x;
+        P(2,i) = y;
+        D(i) = p(3,i);
+    end
 end
